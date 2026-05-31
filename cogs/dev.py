510dbc18ff3db293c7b80116264736da226b12dc
simmu <simmu00@proton.me>
2026-05-31 12:26:57 +0530

    feat(dev): switch sync to app command

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,6 +1,8 @@
 import os
+from typing import Literal
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 
@@ -8,25 +10,31 @@ class Dev(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.is_owner()
-    async def sync(self, ctx: commands.Context, mode: str | None):
+    @app_commands.command(name="sync")
+    async def sync(
+        self, interaction: discord.Interaction, mode: Literal["tree", "cogs"]
+    ):
+        if not await self.bot.is_owner(interaction.user):
+            await interaction.response.send_message(
+                "you are not the owner of this bot >:(", ephemeral=True
+            )
+
         match mode:
             case "tree":
-                await self._sync_tree(ctx)
+                await self._sync_tree(interaction)
             case "cogs":
-                await self._sync_cogs(ctx)
-            case _:
-                await ctx.send("Usage: !sync <tree|cogs>")
+                await self._sync_cogs(interaction)
 
-    async def _sync_tree(self, ctx: commands.Context):
+    async def _sync_tree(self, interaction: discord.Interaction):
         guild = discord.Object(id=os.environ["GUILD_ID"])
 
         self.bot.tree.copy_global_to(guild=guild)
         synced = await self.bot.tree.sync(guild=guild)
-        await ctx.send(f"{len(synced)} app commands synced!")
+        await interaction.response.send_message(
+            f"{len(synced)} app commands synced!", ephemeral=True
+        )
 
-    async def _sync_cogs(self, ctx: commands.Context):
+    async def _sync_cogs(self, interaction: discord.Interaction):
         loaded = set(self.bot.extensions.keys())
         on_disk = {
             f"cogs.{f[:-3]}"
@@ -62,7 +70,7 @@ class Dev(commands.Cog):
             except Exception as e:
                 results.append(f"{ext}: {e}")
 
-        await ctx.send("\n".join(results))
+        await interaction.response.send_message("\n".join(results), ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
